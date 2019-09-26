6c811e44699dc9959591c001d3dc7f92ab7df49c
sych74 <sych74@gmail.com>
2019-09-26 12:24:52 +0300

    Fixed default parameters

--- wordpress/scripts/settings.js
+++ wordpress/scripts/settings.js
@@ -96,7 +96,7 @@ if (group.groupType == 'trial') {
             type: "checkbox",
             name: "cdn-addon",
             caption: cdnText,
-            value: false
+            value: true
         });
     }
 
@@ -106,7 +106,7 @@ if (group.groupType == 'trial') {
             type: "checkbox",
             name: "le-addon",
             caption: sslText,
-            value: false
+            value: true
         });
     }
     settings.fields.push({
