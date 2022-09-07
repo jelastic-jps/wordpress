d948c18934f64cc4f3de2c7c08b1f952a7248c40
sych74 <sych74@gmail.com>
2022-09-07 15:17:42 +0200

    Update getVersion.js

--- cli/wordpress/getVersion.js
+++ cli/wordpress/getVersion.js
@@ -1,1 +1,9 @@
-asd
+//@auth
+//@url(wordpress.getversion)
+//@req(envName)
+
+import com.hivext.api.Response;
+
+include_once com.hivext.scripting.wordpress.local.WordpressAPI;
+
+return new WordPressAPI(envName).getVersion();
