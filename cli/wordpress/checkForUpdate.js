f5f8af52cf4682e59ed8fc90b2ab250b43dd3765
sych74 <sych74@gmail.com>
2022-09-12 15:46:28 +0200

    Delete checkForUpdate.js

--- cli/wordpress/checkForUpdate.js
+++
@@ -1,9 +0,0 @@
-//@auth
-//@url(wordpress.checkforupdate)
-//@req(envName)
-
-import com.hivext.api.Response;
-
-include_once com.hivext.scripting.wordpress.local.WordpressAPI;
-
-return new WordPressAPI(envName).checkForUpdate();
