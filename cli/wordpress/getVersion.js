79ffcd10cd8e5ab8e981dee669b369a51f64281c
sych74 <sych74@gmail.com>
2022-09-12 15:46:53 +0200

    Delete getVersion.js

--- cli/wordpress/getVersion.js
+++
@@ -1,9 +0,0 @@
-//@auth
-//@url(wordpress.getversion)
-//@req(envName)
-
-import com.hivext.api.Response;
-
-include_once com.hivext.scripting.wordpress.local.WordpressAPI;
-
-return new WordPressAPI(envName).getVersion();
