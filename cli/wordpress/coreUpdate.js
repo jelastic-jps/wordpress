36331c987fc4817ceaf03d4245c403de717c5927
sych74 <sych74@gmail.com>
2022-09-12 15:46:37 +0200

    Delete coreUpdate.js

--- cli/wordpress/coreUpdate.js
+++
@@ -1,9 +0,0 @@
-//@auth
-//@url(wordpress.coreupdate)
-//@req(envName)
-
-import com.hivext.api.Response;
-
-include_once com.hivext.scripting.wordpress.local.WordpressAPI;
-
-return new WordPressAPI(envName).coreUpdate();
