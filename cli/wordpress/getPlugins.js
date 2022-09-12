29a6afe38acb093c9802d5d219ecb327adc509af
sych74 <sych74@gmail.com>
2022-09-12 15:46:44 +0200

    Delete getPlugins.js

--- cli/wordpress/getPlugins.js
+++
@@ -1,9 +0,0 @@
-//@auth
-//@url(wordpress.getplugins)
-//@req(envName)
-
-import com.hivext.api.Response;
-
-include_once com.hivext.scripting.wordpress.local.WordpressAPI;
-
-return new WordPressAPI(envName).getPlugins();
