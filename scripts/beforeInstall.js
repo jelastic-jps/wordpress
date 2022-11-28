5979a32f3ba570806ba5ea693b7be926445d0534
sych74 <sych74@gmail.com>
2022-11-28 21:04:33 +0100

    WP-232

--- scripts/beforeInstall.js
+++ scripts/beforeInstall.js
@@ -9,7 +9,7 @@ var resp = {
 if ('${settings.ls-addon:false}'== 'true') {
   resp.nodes.push({
     nodeType: "llsmp",
-    engine: "${settings.php_engine}",
+    engine: "${settings.php_engine:php8.1}",
     count: 1,
     flexibleCloudlets: ${settings.flexibleCloudlets:16},
     fixedCloudlets: ${settings.fixedCloudlets:1},
@@ -26,7 +26,7 @@ if ('${settings.ls-addon:false}'== 'true') {
 } else {
   resp.nodes.push({
     nodeType: "lemp",
-    engine: "${settings.php_engine}",
+    engine: "${settings.php_engine:php8.1}",
     count: 1,
     flexibleCloudlets: ${settings.flexibleCloudlets:16},
     fixedCloudlets: ${settings.fixedCloudlets:1},
